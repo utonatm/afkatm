cacd98f02082000c3e4bbcb5f8b7f681eca9bc97
Mikhail <mikhail.slabodzich@altabel.com>
2019-10-27 15:02:10 +0300

    Minor changes. New Exchange contract address

--- exchange/test/test.js
+++ exchange/test/test.js
@@ -1,14 +1,28 @@
 var AtmExchangeContract = artifacts.require("./AtmExchangeContract.sol");
 
 contract('AtmExchange', accounts => {
+    const srcToken = "0xd0A1E359811322d97991E03f863a0C30C2cF029C";
+    const destToken = "0xC4375B7De8af5a38a93548eb8453a498222C4fF2";
+    const atmAddress = "0xd11f1077efdf72c196c82b243786f88b5e63e3a3";
 
-
-
+    owner = accounts[0];
     it("Get  Exchange rates", async() => {
-        console.log(accounts_num = accounts.length);
-        await AtmExchangeContract.at("0x6f552f400bca3e1a3b772501da5e8fce6e2bb6d1");
+        atmExchangeContract = await AtmExchangeContract.at(atmAddress);
 
+        console.log(accounts.length);
+        atmExchangeContract = await AtmExchangeContract.at(atmAddress);
+        let rate = await atmExchangeContract.getConversionRates.call(srcToken, 1, destToken, { from: owner });
+        console.log('Rate(min,max) =' + rate[0] + ' ' + rate[1]);
     });
+
+    //   it("Execute Swap", async() => {
+    //     atmExchangeContract = await AtmExchangeContract.at("");
+
+
+    //     atmExchangeContract = await AtmExchangeContract.at("");
+    //     let rate = await atmExchangeContract.getConversionRates.call(a1, 1, a2, { from: owner });
+    //     console.log('Rate(min,max) =' + rate[0] + ' ' + rate[1]);
+    // });
 
 
 });
