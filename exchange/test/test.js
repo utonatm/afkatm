f5e1bada1b8bf9be538190bf71db754091344118
Mikhail <mikhail.slabodzich@altabel.com>
2019-10-27 13:45:23 +0300

    Small changes

--- exchange/test/test.js
+++ exchange/test/test.js
@@ -4,9 +4,10 @@ contract('AtmExchange', accounts => {
 
 
 
-    it("should put 10000 MetaCoin in the first account", async() => {
+    it("Get  Exchange rates", async() => {
         console.log(accounts_num = accounts.length);
         await AtmExchangeContract.at("0x6f552f400bca3e1a3b772501da5e8fce6e2bb6d1");
+
     });
 
 
